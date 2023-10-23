bc1d39949931d5d6389699ee30674d49c087ba21
tito <josbert0708@gmil.com>
2023-10-23 15:40:08 -0300

    Creando proyecto dapper

---
+++ Biblioteca/src/Biblio.AdoDapper/AdoDapper.cs
@@ -0,0 +1,14 @@
+﻿using Biblio.Core;
+namespace Biblio.AdoDapper;
+public class AdoDapper : IAdo
+{
+    public void AltaAutor(Autor autor)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Autor> ObtenerAutores()
+    {
+        throw new NotImplementedException();
+    }
+}
