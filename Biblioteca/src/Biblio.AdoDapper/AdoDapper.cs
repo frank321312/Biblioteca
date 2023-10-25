0b10b52e7bbc0661870ff0a34c1ca0d9b4cc0301
tito <josbert0708@gmil.com>
2023-10-25 17:05:31 -0300

    base dedatos

--- Biblioteca/src/Biblio.AdoDapper/AdoDapper.cs
+++ Biblioteca/src/Biblio.AdoDapper/AdoDapper.cs
@@ -1,14 +1,22 @@
-﻿using Biblio.Core;
+﻿using System.Data;
+using Biblio.Core;
+using Dapper;
+using MySqlConnector;
+
 namespace Biblio.AdoDapper;
 public class AdoDapper : IAdo
 {
+    private readonly IDbConnection _conexion;
+    private readonly string _queryAutores 
+        ="SELECT * FROM Autor ORDER BY apellido ASC,nombre ASC";
+    public AdoDapper(IDbConnection conexion) => this._conexion = conexion;
+    public AdoDapper(string cadena)
+      =>  _conexion = new MySqlConnection(cadena);
     public void AltaAutor(Autor autor)
     {
         throw new NotImplementedException();
     }
 
     public List<Autor> ObtenerAutores()
-    {
-        throw new NotImplementedException();
-    }
+    =>_conexion.Query<Autor>(_queryAutores).ToList();
 }
