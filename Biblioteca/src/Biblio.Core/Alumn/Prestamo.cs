9d4997960f7d11f3e7beec89ba39ff0234812c73
josbert <josbert0708@gmail.com>
2023-11-12 18:25:25 -0300

    si

--- Biblioteca/src/Biblio.Core/Alumn/Prestamo.cs
+++ Biblioteca/src/Biblio.Core/Alumn/Prestamo.cs
@@ -4,7 +4,7 @@ public class Prestamo
 {
     public required byte NumeroCopia { get; set; }
     public required uint Dni { get; set; }
-    public required ulong Isbn { get; set; }
+    public required ulong ISBN { get; set; }
     public required DateTime FechaEgreso { get; set; }
     public required DateTime FechaRegreso { get; set; }
 }
--- Biblioteca/src/Biblio.Core/Alumn/Solicitud.cs
+++ Biblioteca/src/Biblio.Core/Alumn/Solicitud.cs
@@ -3,7 +3,7 @@ namespace Biblio.Core;
 public class Solicitud
 {
     public required uint IdSolicitud { get; set; }
-    public required uint Dni { get; set; }
+    public required Alumno Dni { get; set; }
     public required ulong ISBN { get; set; }
     public required DateTime FechaSolicitud { get; set; }
 }
