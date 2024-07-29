17128d8f08770e67b43b2866e86c8a15806b0a1b
JRyoy <josbert0708@gmail.com>
2024-07-29 17:26:51 -0300

    async

--- Biblioteca/src/Biblio.AdoDapper/AdoDapper.cs
+++ Biblioteca/src/Biblio.AdoDapper/AdoDapper.cs
@@ -1,4 +1,5 @@
 ﻿﻿using System.Data;
+using System.Runtime.CompilerServices;
 using Biblio.Core;
 using Dapper;
 using MySqlConnector;
@@ -28,49 +29,70 @@ public class AdoDapper : IAdo
     private static readonly string _queryAlumno
         = "SELECT nombre ,apellido ,celular ,email ,DNI ,idCurso FROM Alumno ORDER BY Dni ASC";
 
-    #region Autor
-    public void AltaAutor(Autor autor)
+
+    #region AutorAsync
+    public async Task AltaAutorAsync(Autor autor)
+    {
+        DynamicParameters parametros = ParametrosParaAltaAutor(autor);
+
+        await _conexion.ExecuteAsync("altaAutor", parametros, commandType: CommandType.StoredProcedure);
+
+        autor.IdAutor = parametros.Get<ushort>("unIdAutor");
+    }
+
+    private static DynamicParameters ParametrosParaAltaAutor(Autor autor)
     {
         var parametros = new DynamicParameters();
         parametros.Add("@unIdAutor", direction: ParameterDirection.Output);
         parametros.Add("@unNombre", autor.Nombre);
         parametros.Add("@unApellido", autor.Apellido);
-
-        _conexion.Execute("altaAutor", parametros, commandType: CommandType.StoredProcedure);
-
-        autor.IdAutor = parametros.Get<ushort>("unIdAutor");
+        return parametros;
     }
 
-    public List<Autor> ObtenerAutores()
-        => _conexion.Query<Autor>(_queryAutores).ToList();
+    public async Task<List<Autor>> ObtenerAutoresAsync()
+        => (await _conexion.QueryAsync<Autor>(_queryAutores)).ToList();
 
     #endregion
 
-    #region Editorial
-    public void AltaEditorial(Editorial editorial)
+    #region EditorialAsync
+    public async Task AltaEditorialAsync(Editorial editorial)
+    {
+        DynamicParameters parametros = ParametrosParaAltaEditorial(editorial);
+
+        await _conexion.ExecuteAsync("altaEditorial", parametros, commandType: CommandType.StoredProcedure);
+    }
+
+    private static DynamicParameters ParametrosParaAltaEditorial(Editorial editorial)
     {
         var parametros = new DynamicParameters();
         parametros.Add("@unIdEditorial", direction: ParameterDirection.Output);
         parametros.Add("@unNombre", editorial.Nombre);
-
-        _conexion.Execute("altaEditorial", parametros, commandType: CommandType.StoredProcedure);
+        return parametros;
     }
-    public List<Editorial> ObtenerEditorial()
-        => _conexion.Query<Editorial>(_queryEditorial).ToList();
+
+    public async Task<List<Editorial>> ObtenerEditorialAsync()
+        => (await _conexion.QueryAsync<Editorial>(_queryEditorial)).ToList();
     #endregion
 
-    #region Libro
-    public void AltaLibro(Libro libro)
+    #region LibroAsync
+    public async Task AltaLibroAsync(Libro libro)
+    {
+        DynamicParameters parametros = ParametrosParaAltaLibro(libro);
+
+        await _conexion.ExecuteAsync("altaLibro", parametros, commandType: CommandType.StoredProcedure);
+    }
+
+    private static DynamicParameters ParametrosParaAltaLibro(Libro libro)
     {
         var parametros = new DynamicParameters();
         parametros.Add("@unISBN", libro.ISBN);
         parametros.Add("@unIdTitulo", libro.Titulo.IdTitulo);
         parametros.Add("@unIdEditorial", libro.Editorial.IdEditorial);
-        
-        _conexion.Execute("altaLibro", parametros, commandType: CommandType.StoredProcedure);
+        return parametros;
     }
-    public List<Libro> ObtenerLibro()
-        => _conexion.Query<Libro>(_queryLibro).ToList();
+
+    public async Task<List<Libro>> ObtenerLibroAsync()
+        => (await _conexion.QueryAsync<Libro>(_queryLibro)).ToList();
     /*
     public void ObtenerLibroPorISBN(ulong isbn)
     {
@@ -90,23 +112,22 @@ public class AdoDapper : IAdo
             INNER JOIN Editorial ON Libro.idEditorial = Editorial.idEditorial
             WHERE   ISBN = @unIsbn
             LIMIT 1";
-    public Libro? ObtenerLibroPorISBN(ulong isbn)
+    public async Task<Libro?> ObtenerLibroPorISBNAsync(ulong isbn)
     {
-        var libro = _conexion.Query<Libro, Titulo, Editorial, Libro>
-            (_queryLibroISBN, 
-                (libro, titulo, editorial) => 
+        var libro = (await _conexion.QueryAsync<Libro, Titulo, Editorial, Libro>
+            (_queryLibroISBN,
+                (libro, titulo, editorial) =>
                     {
                         libro.Titulo = titulo;
                         libro.Editorial = editorial;
 
                         return libro;
                     },
-                new {unIsbn = isbn},
-                splitOn: "idTitulo, idEditorial").
+                new { unIsbn = isbn },
+                splitOn: "idTitulo, idEditorial")).
                 FirstOrDefault();
         if (libro is not null)
-            libro.Titulo.Autores = ObtenerAutorPorISBN(isbn).Titulo.Autores;
-
+            libro.Titulo.Autores = (await ObtenerAutorPorISBNAsync(isbn)).Titulo.Autores;
         return libro;
     }
 
@@ -122,15 +143,15 @@ public class AdoDapper : IAdo
             JOIN Autor USING (idAutor)
             WHERE 	ISBN = @unIsbn
             ORDER BY Autor.apellido ASC, Autor.nombre ASC";
-    public Libro? ObtenerAutorPorISBN(ulong isbn)
+    public async Task<Libro?> ObtenerAutorPorISBNAsync(ulong isbn)
     {
-        var libro = _conexion.Query<Libro, Titulo, Editorial, Autor, Libro>
+        var libro = (await _conexion.QueryAsync<Libro, Titulo, Editorial, Autor, Libro>
             (_queryAutorISBN,
-                (libro, titulo, editorial, autor) => 
+                (libro, titulo, editorial, autor) =>
                     {
                         libro.Titulo = titulo;
                         libro.Editorial = editorial;
-                        
+
                         if (titulo.Autores == null)
                         {
                             titulo.Autores = new List<Autor>();
@@ -140,23 +161,193 @@ public class AdoDapper : IAdo
 
                         return libro;
                     },
-                    new {unIsbn = isbn},
+                    new { unIsbn = isbn },
+                    splitOn: "idTitulo, idEditorial, idAutor")).
+                    FirstOrDefault();
+
+        return libro;
+    }
+    #endregion
+
+    #region TituloAsync
+    private readonly string queryInsertAutorTitulo
+    = "	INSERT INTO AutorTitulo(idTitulo, idAutor)VALUES (@unIdTitulo, @unIdAutor)";
+    public async Task AltaTituloAsync(Titulo titulo)
+    {
+        var parametros = ParametrosParaAltaTitulo(titulo);
+
+        _conexion.Open();
+        using (var transaccion = _conexion.BeginTransaction())
+        {
+            try
+            {
+                await _conexion.ExecuteAsync("altaTitulo", parametros, commandType: CommandType.StoredProcedure, transaction: transaccion);
+                titulo.IdTitulo = parametros.Get<uint>("@unIdTitulo");
+
+                var paraTitulo = titulo.Autores.
+                    Select(a => new { unIdTitulo = titulo.IdTitulo, unIdAutor = a.IdAutor }).
+                    ToList();
+
+                await _conexion.ExecuteAsync(queryInsertAutorTitulo, paraTitulo, transaction: transaccion);
+
+                //Como todo se ejecuto ok, confirmo los cambios
+                transaccion.Commit();
+            }
+            catch (MySqlException e)
+            {
+                //Si hubo algun problema, doy marcha atras con los posibles cambios
+                transaccion.Rollback();
+                throw new InvalidOperationException(e.Message, e);
+            }
+        }
+    }
+    public async Task<List<Titulo>> ObtenerTituloAsync()
+        => (await _conexion.QueryAsync<Titulo>(_queryTitulo)).ToList();
+
+    #endregion
+
+    #region FueraDeCirculacionAsync
+    public async Task AltaFueraDeCirculacionAsync(FueraCirculacion fueraCirculacion, Libro libro)
+    {
+        DynamicParameters parametros = ParametrosParaAltaFueraDeCirculacion(fueraCirculacion, libro);
+        await _conexion.ExecuteAsync("altaFueraDeCirculacion", parametros, commandType: CommandType.StoredProcedure);
+    }
+
+    private static DynamicParameters ParametrosParaAltaFueraDeCirculacion(FueraCirculacion fueraCirculacion, Libro libro)
+    {
+        var parametros = new DynamicParameters();
+        parametros.Add("@unNumeroCopia", direction: ParameterDirection.Output);
+        parametros.Add("@unISBN", libro.ISBN);
+        parametros.Add("@unFechaSalida", fueraCirculacion.FechaSalida);
+        return parametros;
+    }
+
+    public async Task<List<FueraCirculacion>> ObtenerFueraDeCirculacionAsync()
+        => (await _conexion.QueryAsync<FueraCirculacion>(_queryFueraDeCirculacion)).ToList();
+    #endregion
+
+    #region CursoAsync
+    public async Task AltaCursoAsync(Curso curso)
+    {
+        var parametros = ParametrosAltaCurso(curso);
+        await _conexion.ExecuteAsync("altaCurso", parametros, commandType: CommandType.StoredProcedure);
+        curso.IdCurso = parametros.Get<byte>("unIdCurso");
+    }
+    public async Task<List<Curso>> ObtenerCursoAsync()
+        => (await _conexion.QueryAsync<Curso>(_queryCurso)).ToList();
+
+    #endregion
+
+    #region  AlumnoAsync
+    public async Task AltaAlumnoAsync(Alumno alumno, string pass)
+    {
+        DynamicParameters parametros = ParametrosParaAltaAlumno(alumno, pass);
+        await _conexion.ExecuteAsync("altaAlumno", parametros, commandType: CommandType.StoredProcedure);
+    }
+
+    private static DynamicParameters ParametrosParaAltaAlumno(Alumno alumno, string pass)
+    {
+        var parametros = new DynamicParameters();
+        parametros.Add("@unDni", alumno.Dni);
+        parametros.Add("@unNombre ", alumno.Nombre);
+        parametros.Add("@unApellido", alumno.Apellido);
+        parametros.Add("@unCelular ", alumno.Celular);
+        parametros.Add("@unEmail", alumno.Email);
+        parametros.Add("@unContraseña", pass);
+        parametros.Add("@unIdCurso", alumno.IdCurso);
+        return parametros;
+    }
+
+    public async Task<List<Alumno>> ObtenerAlumnosAsync()
+        => (await _conexion.QueryAsync<Alumno>(_queryAlumno)).ToList();
+    #endregion
+
+    #region Autor
+    public void AltaAutor(Autor autor)
+    {
+        var parametros = ParametrosParaAltaAutor(autor);
+
+        _conexion.Execute("altaAutor", parametros, commandType: CommandType.StoredProcedure);
+
+        autor.IdAutor = parametros.Get<ushort>("unIdAutor");
+    }
+
+    public List<Autor> ObtenerAutores()
+        => _conexion.Query<Autor>(_queryAutores).ToList();
+
+    #endregion
+
+    #region Editorial
+    public void AltaEditorial(Editorial editorial)
+    {
+        var parametros = ParametrosParaAltaEditorial(editorial);
+
+        _conexion.Execute("altaEditorial", parametros, commandType: CommandType.StoredProcedure);
+    }
+    public List<Editorial> ObtenerEditorial()
+        => _conexion.Query<Editorial>(_queryEditorial).ToList();
+    #endregion
+
+    #region Libro
+    public void AltaLibro(Libro libro)
+    {
+        var parametros = ParametrosParaAltaLibro(libro);
+
+        _conexion.Execute("altaLibro", parametros, commandType: CommandType.StoredProcedure);
+    }
+    public List<Libro> ObtenerLibro()
+        => _conexion.Query<Libro>(_queryLibro).ToList();
+
+    public Libro? ObtenerLibroPorISBN(ulong isbn)
+    {
+        var libro = _conexion.Query<Libro, Titulo, Editorial, Libro>
+            (_queryLibroISBN,
+                (libro, titulo, editorial) =>
+                    {
+                        libro.Titulo = titulo;
+                        libro.Editorial = editorial;
+
+                        return libro;
+                    },
+                new { unIsbn = isbn },
+                splitOn: "idTitulo, idEditorial").
+                FirstOrDefault();
+        if (libro is not null)
+            libro.Titulo.Autores = ObtenerAutorPorISBN(isbn).Titulo.Autores;
+
+        return libro;
+    }
+    public Libro? ObtenerAutorPorISBN(ulong isbn)
+    {
+        var libro = _conexion.Query<Libro, Titulo, Editorial, Autor, Libro>
+            (_queryAutorISBN,
+                (libro, titulo, editorial, autor) =>
+                    {
+                        libro.Titulo = titulo;
+                        libro.Editorial = editorial;
+
+                        if (titulo.Autores == null)
+                        {
+                            titulo.Autores = new List<Autor>();
+                        }
+
+                        titulo.Autores.Add(autor);
+
+                        return libro;
+                    },
+                    new { unIsbn = isbn },
                     splitOn: "idTitulo, idEditorial, idAutor").
                     FirstOrDefault();
-        
+
         return libro;
     }
     #endregion
 
     #region Titulo
-    private readonly string queryInsertAutorTitulo
-    = "	INSERT INTO AutorTitulo(idTitulo, idAutor)VALUES (@unIdTitulo, @unIdAutor)";
+
     public void AltaTitulo(Titulo titulo)
     {
-        var parametros = new DynamicParameters();
-        parametros.Add("@unPublicacion", titulo.Publicacion);
-        parametros.Add("@unTitulo", titulo.titulo);
-        parametros.Add("@unIdTitulo", direction: ParameterDirection.Output);
+        DynamicParameters parametros = ParametrosParaAltaTitulo(titulo);
 
         _conexion.Open();
         using (var transaccion = _conexion.BeginTransaction())
@@ -183,6 +374,16 @@ public class AdoDapper : IAdo
             }
         }
     }
+
+    private static DynamicParameters ParametrosParaAltaTitulo(Titulo titulo)
+    {
+        var parametros = new DynamicParameters();
+        parametros.Add("@unPublicacion", titulo.Publicacion);
+        parametros.Add("@unTitulo", titulo.titulo);
+        parametros.Add("@unIdTitulo", direction: ParameterDirection.Output);
+        return parametros;
+    }
+
     public List<Titulo> ObtenerTitulo()
         => _conexion.Query<Titulo>(_queryTitulo).ToList();
 
@@ -191,10 +392,8 @@ public class AdoDapper : IAdo
     #region FueraDeCirculacion
     public void AltaFueraDeCirculacion(FueraCirculacion fueraCirculacion, Libro libro)
     {
-        var parametros = new DynamicParameters();
-        parametros.Add("@unNumeroCopia", direction: ParameterDirection.Output);
-        parametros.Add("@unISBN", libro.ISBN);
-        parametros.Add("@unFechaSalida", fueraCirculacion.FechaSalida);
+        var parametros = ParametrosParaAltaFueraDeCirculacion(fueraCirculacion, libro);
+        _conexion.Execute("altaFueraDeCirculacion", parametros, commandType: CommandType.StoredProcedure);
     }
 
     public List<FueraCirculacion> ObtenerFueraDeCirculacion()
@@ -204,13 +403,20 @@ public class AdoDapper : IAdo
     #region Curso
     public void AltaCurso(Curso curso)
     {
+        DynamicParameters parametros = ParametrosAltaCurso(curso);
+        curso.IdCurso = parametros.Get<byte>("unIdCurso");
+    }
+
+    private DynamicParameters ParametrosAltaCurso(Curso curso)
+    {
         var parametros = new DynamicParameters();
         parametros.Add("@unIdCurso", direction: ParameterDirection.Output);
         parametros.Add("@unanio", curso.anio);
         parametros.Add("@unDivision", curso.Division);
         _conexion.Execute("altaCurso", parametros, commandType: CommandType.StoredProcedure);
-        curso.IdCurso = parametros.Get<byte>("unIdCurso");
+        return parametros;
     }
+
     public List<Curso> ObtenerCurso()
         => _conexion.Query<Curso>(_queryCurso).ToList();
 
@@ -219,19 +425,10 @@ public class AdoDapper : IAdo
     #region  Alumno
     public void AltaAlumno(Alumno alumno, string pass)
     {
-        var parametros = new DynamicParameters();
-        parametros.Add("@unDni", alumno.Dni);
-        parametros.Add("@unNombre ", alumno.Nombre);
-        parametros.Add("@unApellido", alumno.Apellido);
-        parametros.Add("@unCelular ", alumno.Celular);
-        parametros.Add("@unEmail", alumno.Email);
-        parametros.Add("@unContraseña", pass);
-        parametros.Add("@unIdCurso", alumno.IdCurso);
+        var parametros = ParametrosParaAltaAlumno(alumno, pass);
         _conexion.Execute("altaAlumno", parametros, commandType: CommandType.StoredProcedure);
     }
     public List<Alumno> ObtenerAlumnos()
         => _conexion.Query<Alumno>(_queryAlumno).ToList();
     #endregion
-
-
 }
--- Biblioteca/src/Biblio.Core/IAdo.cs
+++ Biblioteca/src/Biblio.Core/IAdo.cs
@@ -2,7 +2,6 @@ namespace Biblio.Core;
 
 public interface IAdo
 {
-
     void AltaAutor(Autor autor);
     List<Autor>ObtenerAutores();
     
@@ -26,4 +25,30 @@ public interface IAdo
 
     Libro? ObtenerLibroPorISBN(ulong isbn);
     Libro? ObtenerAutorPorISBN(ulong isbn);
+    #region Methodasync
+    Task AltaAutorAsync(Autor autor);
+    Task<List<Autor>> ObtenerAutoresAsync();
+    
+    Task AltaEditorialAsync(Editorial editorial);
+    Task<List<Editorial>> ObtenerEditorialAsync();
+
+
+    Task AltaLibroAsync(Libro libro);
+    Task<List<Libro>>ObtenerLibroAsync();
+
+    Task AltaTituloAsync(Titulo titulo);
+    Task<List<Titulo>>ObtenerTituloAsync();
+    
+    Task AltaFueraDeCirculacionAsync(FueraCirculacion fueraCirculacion,Libro libro);
+    Task<List<FueraCirculacion>> ObtenerFueraDeCirculacionAsync();
+    
+    Task AltaCursoAsync(Curso curso); 
+    Task<List<Curso>>ObtenerCursoAsync();
+    
+    Task AltaAlumnoAsync(Alumno alumno, string pass);
+    Task<List<Alumno>> ObtenerAlumnosAsync();    
+
+    Task<Libro?> ObtenerLibroPorISBNAsync(ulong isbn);
+    Task<Libro?> ObtenerAutorPorISBNAsync(ulong isbn);
+    #endregion
 }
