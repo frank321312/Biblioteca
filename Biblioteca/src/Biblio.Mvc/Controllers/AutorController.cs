1d5e0999284f4e820897e7bd9319e5c79aa3ab03
JRyoy <josbert0708@gmail.com>
2024-08-26 17:09:50 -0300

    Autor

--- Biblioteca/src/Biblio.Mvc/Controllers/AutorController.cs
+++ Biblioteca/src/Biblio.Mvc/Controllers/AutorController.cs
@@ -3,13 +3,16 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Biblio.Mvc.Controllers;
 public class AutorController : Controller
-{    protected readonly IAdo Ado;
+{
+    protected readonly IAdo Ado;
     private static readonly string _cadena =
-        @"Server=localhost;Database=5to_Biblioteca;Uid=root;pwd=root;Allow User Variables=True";
-    public AutorController() 
+        @"Server=localhost;Database=5to_Biblioteca;Uid=5to_agbd;pwd=Trigg3rs!;Allow User Variables=True";
+    public AutorController()
     {
         Ado = new Biblio.AdoDapper.AdoDapper(_cadena);
     }
+
+    [HttpGet]
     public async Task<IActionResult> ObtenerAutores()
     {
         var autores = await Ado.ObtenerAutoresAsync();
@@ -19,10 +22,17 @@ public class AutorController : Controller
         return View("../Author/Autor", autor);
     }
 
+    [HttpGet]
+    public IActionResult GetAltaAutor()
+    {
+        return View("../Author/AltaAutor");
+    }
+
     [HttpPost]
     public async Task<IActionResult> AltaAutor(Autor autor)
     {
         await Ado.AltaAutorAsync(autor);
-        return RedirectToAction(nameof(ObtenerAutores));
+        // return RedirectToAction(nameof(ObtenerAutores));
+        return View("../Author/AltaAutor");
     }
 }
