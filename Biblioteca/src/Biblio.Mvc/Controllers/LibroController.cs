7bc8e5e854b632ff1e90663be3aed76321972aaa
JRyoy <josbert0708@gmail.com>
2024-09-04 15:26:05 -0300

    librocontroller

---
+++ Biblioteca/src/Biblio.Mvc/Controllers/LibroController.cs
@@ -0,0 +1,27 @@
+﻿using Biblio.Core;
+using Biblio.Mvc.Controllers.Modal;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Biblio.Mvc.Controllers
+{
+    public class LibroController:Controller
+    {
+        protected readonly IAdo Ado;
+        private static readonly string _cadena =
+            // @"Server=localhost;Database=5to_Biblioteca;Uid=5to_agbd;pwd=Trigg3rs!;Allow User Variables=True";
+            @"Server=localhost;Database=5to_Biblioteca;Uid=root;pwd=root;Allow User Variables=True";
+        public LibroController()
+        {
+            Ado = new Biblio.AdoDapper.AdoDapper(_cadena);
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetObtenerLibro()
+        {
+            var libro = await Ado.ObtenerLibroAsync();
+            var ordenarlibro = libro.OrderBy(x => x.ISBN).ToList();
+            return View("../Title/AltaTitulo",ordenarlibro);
+        }
+        
+
+    }
+}
---
+++ Biblioteca/src/Biblio.Mvc/Controllers/Modal/LibroModal.cs
@@ -0,0 +1,20 @@
+﻿using Biblio.Core;
+
+namespace Biblio.Mvc.Controllers.Modal
+{
+    public class LibroModal
+    {
+
+        public Titulo Titulo { get; set; }
+        public Editorial Editorial { get; set; }
+        public ulong ISBN { get; set; }
+
+        //List<Solicitud> Solicitudes { get; set; }
+        //List<FueraCirculacion> FueraCirculaciones { get; set; }
+        //List<Prestamo> Prestamos { get; set; }
+        public LibroModal()
+        {
+        }
+
+    }
+}
